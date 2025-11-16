db85ce0bfd0232723c1608db63c18d65818d1472
roberto_esaclear <roberto.delprete@esa.int>
2025-11-16 14:44:39 +0100

    time

--- docs/countdown.js
+++ docs/countdown.js
@@ -1,4 +1,4 @@
-const launchDate = new Date('Jul 7, 2025 23:59:59').getTime();
+const launchDate = new Date('Apr 18, 2026 23:59:59').getTime();
 
 const countdown = function () {
   const presentDate = new Date().getTime();
